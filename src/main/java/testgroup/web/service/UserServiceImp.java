cbd0dacd0664eb4f957c12032dc5b29867705b6d
Konstantin <lord-konst@mail.ru>
2022-01-07 18:25:18 +0300

    4 commit

--- src/main/java/testgroup/web/service/UserServiceImp.java
+++ src/main/java/testgroup/web/service/UserServiceImp.java
@@ -2,45 +2,44 @@ package testgroup.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 import testgroup.web.model.User;
 import testgroup.web.repositories.UserRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImp implements UserService {
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    @Transactional
     public List<User> allUsers() {
         return userRepository.findAll();
     }
 
     @Override
-    @Transactional
     public void add(User user) {
         userRepository.save(user);
     }
 
     @Override
-    @Transactional
     public void delete(User user) {
         userRepository.delete(user);
     }
 
     @Override
-    @Transactional
     public void edit(User user) {
         userRepository.save(user);
     }
 
     @Override
-    @Transactional
+
     public User getById(Integer id) {
-        return userRepository.getById(id);
+        return userRepository.findById(id).orElseThrow();
+
     }
 }
